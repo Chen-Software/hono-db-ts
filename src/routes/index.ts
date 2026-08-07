e8a5cebfe86d1901b5ca48781a5849147cb2e982
Yumin Chen <yumin@chen.so>
2026-08-07 04:47:14 +0100

    Update routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,7 @@
-export * from "./movies"
+import type { Hono } from "hono";
+import * as movies from "./movies";
+import { repos } from "@/repo/repos";
+
+export function createAllRoutes(app: Hono) {
+	repos["movies"] && movies.createRoutes(app, repos["movies"]);
+}
