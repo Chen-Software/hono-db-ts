8d3e19df67be7458068885dc4ad272bbf2efb743
Yumin Chen <yumin@chen.so>
2026-08-09 15:26:50 +0100

    Decouple app service from user service

--- src/services/app-service.ts
+++ src/services/app-service.ts
@@ -1,17 +1,9 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
-import { UserService } from "./user-service";
 
 const AppService = new Hono();
 
 AppService.get("/", (c) => c.json({ status: "ok" }));
-
-AppService.get("/info", (c) =>
-	c.json({ status: "ok", service: "web-service" }),
-);
-
-// the app service depends on the user service for user management
-AppService.route("/users", UserService);
 
 AppService.onError((err, c) => {
 	console.error(err);
