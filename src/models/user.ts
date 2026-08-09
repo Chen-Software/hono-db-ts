d1e7cc6a4007ad1cfa10bd0a03a7c5057db9511c
Yumin Chen <yumin@chen.so>
2026-08-09 14:32:26 +0100

    Add protobuf API to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,10 +2,11 @@ import typia, { tags } from "typia";
 import type { UUID } from "crypto";
 import { type Identifiable } from "../capacities/identifiable";
 
-export const validateMember = typia.createValidate<User>();
-
 interface User extends Identifiable<UUID> {
-	email: string & tags.Format<"email">;
+	name: string & tags.MinLength<1> & tags.MaxLength<100>;
+	email: string & tags.Format<"email"> & tags.MaxLength<255>;
+	role: "admin" | "member" | "viewer";
+	created_at: string & tags.Format<"date-time">;
 	age: number &
 		tags.Type<"uint32"> &
 		tags.ExclusiveMinimum<19> &
@@ -14,10 +15,12 @@ interface User extends Identifiable<UUID> {
 
 const UserModel = {
 	is: typia.createIs<User>(),
-	equals: typia.createValidateEquals<User>(),
+	equals: typia.createAssertEquals<User>(),
 	assert: typia.createAssert<User>(),
-	assertEquals: typia.createAssertEquals<User>(),
-	validates: typia.createValidate<User>(),
+    validate: typia.createValidate<User>(),
+    encode: typia.protobuf.createAssertEncode<User>(),
+    decode: typia.protobuf.createAssertDecode<User>(),
+    message: typia.protobuf.message<User>()
 };
 
 export { type User, UserModel };
