8584c0522112411a920c94a3e83486f1fae27df0
Yumin Chen <yumin@chen.so>
2026-08-09 15:46:46 +0100

    Remove instantiable

--- src/capacities/instantiable.ts
+++
@@ -1,8 +0,0 @@
-import { type Classifiable } from "typia";
-
-interface Instantiable<Model> {
-	new (...args: any[]): Model;
-	from(seed: Classifiable<Model>): Model;
-}
-
-export { type Instantiable };
