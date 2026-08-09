73e2b374e6de0d81e5b339c9810c79fa38d12691
Yumin Chen <yumin@chen.so>
2026-08-09 13:32:57 +0100

    Install bun and add WebService

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import { WebService } from "./services/web-service";
+
+const app = WebService;
+
+const PORT = Number(Bun.env["PORT"] ?? 8080);
+
+console.log(`Serving web-service on http://localhost:${PORT}`);
+
+export default {
+	port: PORT,
+	fetch: app.fetch,
+};
