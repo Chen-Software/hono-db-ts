8fec4e6e5a87b36387cdc836d1513813f7e593e5
Yumin Chen <yumin@chen.so>
2026-08-09 14:09:28 +0100

    Update identifiable capacity

--- src/capacities/identifiable.ts
+++ src/capacities/identifiable.ts
@@ -1,10 +1,11 @@
-import typia, { tags } from "typia";
+import typia, { type tags } from "typia";
+import { type UUID } from "crypto";
 
-interface Identifiable<T extends "uuid"> {
-	id: string & tags.Format<T>;
+interface Identifiable<T extends UUID> {
+	id: T & tags.Format<"uuid">;
 }
 
-const isIdentifiable = typia.createIs<Identifiable<"uuid">>();
-const validateIdentifiable = typia.createValidate<Identifiable<"uuid">>();
+const isIdentifiable = typia.createIs<Identifiable<UUID>>();
+const validateIdentifiable = typia.createValidate<Identifiable<UUID>>();
 
 export { type Identifiable, isIdentifiable, validateIdentifiable };
